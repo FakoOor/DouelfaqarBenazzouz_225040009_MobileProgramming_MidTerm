ed7c9870914e5b3a7b85e80d44a76edb6f4b4262
Dou Elfaqar Benazzouz <149887845+FakoOor@users.noreply.github.com>
2024-05-26 21:02:20 +0300

    Add files via upload

---
+++ faker-main/Combat.cs
@@ -0,0 +1,15 @@
+﻿using DouElfaqarBenazzouz_225040009_GameProgramming;
+using System;
+
+public class Combat
+{
+	private Game _theGame;
+
+	public Player Player { get; private set; }
+
+    public Combat(Game game)
+	{
+		_theGame = game;
+		Player = game.Player;
+	}
+}
